f45fe83d5e9121bdcb163c0eef82ae8db7249a00
user <user@Huyen-MacBook-Air.local>
2026-05-17 11:29:59 +0700

    Notifilecation System

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,12 @@
-import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
 
 export default function HomePage() {
   return (
     <PageContainer title="Trang chủ">
-      <div>Chào mừng bạn đến với nên tảng quản lí giải đấu thể thao điện tử</div>
+      <div style={{ padding: '20px', background: '#fff', borderRadius: '8px' }}>
+        Chào mừng bạn đến với nền tảng quản lý giải đấu thể thao điện tử
+      </div>
     </PageContainer>
   );
 }
